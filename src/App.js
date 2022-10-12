2bf18c9d3fe72e8a2e59134882642c1064a2799c
Rbn-Sanchez <A01378379@itesm.mx>
2022-10-12 17:18:01 -0500

    Página web

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import RecordMenu from './components/RecordMenu';
 
--- src/components/RecordMenu.js
+++ src/components/RecordMenu.js
@@ -1,23 +1,23 @@
-import { Button, Grid, Paper } from "@mui/material";
-import { Container } from "@mui/system";
+import { Button, ButtonGroup , Paper} from '@mui/material'
+import { Box, Container, Stack } from '@mui/system';
 import '../style/RecordMenu.css'
 
 
 const RecordMenu = () => {
     return (
-        <Container sx={{ width: 1/2}}>
-            <Grid container spacing = {1}>
-                <Grid item xs = {12}>
-                    <Paper className="audio-interface">Chat bot</Paper>
-                </Grid>
-                <Grid item xs = {6}>
-                        <Button variant="contained" className="button">Hablar</Button>
-                </Grid>
-                <Grid item xs = {6}>
-                        <Button variant="contained" className="button">Detener</Button>
-                </Grid>
-            </Grid> 
-        </Container>
+            <Container sx={{ width: '25%' , minWidth: 200}}>
+                <Stack spacing={1}>
+                    <Box textAlign='center'>
+                        <Paper sx={{ height: 400 }}>hola</Paper>
+                    </Box>
+                    <Box textAlign='center'>
+                            <ButtonGroup variant="contained">
+                                <Button >Hablar</Button>
+                                <Button>Detener</Button>
+                            </ButtonGroup>
+                    </Box>
+                </Stack>
+            </Container>
     );
 }
 
