ea17577612dc61d3a811b69d96a71b6ac109f44c
Carlos Avila <a01750220@tec.mx>
2022-11-18 20:21:58 -0600

    frontend v.3
    Frontend v.3

--- src/components/input.js
+++ src/components/input.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "../chat.scss"
 
 export const Input = () => {
     return(
         <div className="input">
-            
+            <input type="text" placeholder="Escribe algo..." />
+            <div className="send">
+                <button>Enviar</button>
+                <button>Grabar</button>
+            </div>
         </div>
     )
 }
--- src/components/message.js
+++ src/components/message.js
@@ -2,6 +2,15 @@ import React from "react";
 
 export const Message = () => {
     return(
-        <div>Message</div>
+        <div className="message owner">
+            <div className="messageInfo">
+                <img src="https://cdn.dribbble.com/users/279657/screenshots/2701628/chatbot.png" alt="hera"/>
+                <span>Ahora</span>
+            </div>
+            <div className="messageContent">
+                <p>Hola soy Hera, ¿en qué puedo ayudarte?</p>
+                <img src="" alt=""/>
+            </div>
+        </div>
     )
 }
